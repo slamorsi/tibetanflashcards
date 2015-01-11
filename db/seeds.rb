577f9e2c0c2d7396b8d91d617811a2de82650f8f
Islam Morsi <morsislam@gmail.com>
2015-01-11 15:00:56 -0800

    seeding with alphabet

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,11 @@
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 # Environment variables (ENV['...']) can be set in the file .env file.
+
+
+tibetanChars = %w(ཀ ཁ ག ང ཅ ཆ ཇ ཉ ཏ ཐ ད ན པ ཕ བ མ ཙ ཚ ཛ ཝ ཞ ཟ འ ཡ ར ལ ཤ ས ཧ ཨ)
+translations = %w(A B C D E F G H I J K L M N O P Q R S T U V X Y Z AA BB CC DD)
+
+tibetanChars.each_with_index {|item, index|
+  OriginalCard.create(character: item, translation_card_attributes: {character: translations[index]})  
+}
